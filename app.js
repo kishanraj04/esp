59d55947802773f9ffc7cce6f2d8eb4940e2ed7d
kishanraj04 <kishuraj1111@gmail.com>
2026-02-16 07:15:56 +0530

    adding some events

--- app.js
+++ app.js
@@ -2,67 +2,46 @@ import express from "express";
 import http from "http";
 import { router1 } from "./src/router/router1.js";
 import "./db/conntectdb.js";
-import { WebSocketServer } from "ws";
+import cors from "cors"; 
+import { Server } from "socket.io";
 
 const app = express();
 const PORT = 3000;
 
 // ✅ Create HTTP server from express app
 const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } }); 
+
+io.on("connection", (socket) => {
+  console.log("New client connected:", socket.id);
+
+  socket.emit("message","welcome to the server");
+
+  socket.on("deviceSelected", (data) => {
+    const { deviceId, roomId } = data;
+
+    //emit the roomid and device id to esp32 and catch the data in esp32 and perform the action
+    io.emit("deviceSelected", { deviceId, roomId });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", router1);
+app.use(cors()); 
 
 app.get("/", (req, res) => {
   res.send("Server is running 🚀");
 });
 
-// ✅ WebSocket should use HTTP server (NOT app)
-const wss = new WebSocketServer({ server });
 
-wss.on("connection", (ws) => {
 
-  console.log("New client connected");
 
-  ws.on("message", (message) => {
-
-    try {
-      const data = JSON.parse(message.toString());
-
-      // 🔥 Check if ESP32
-      if (data.type === "device") {
-        ws.deviceId = data.id;
-
-        console.log("✅ ESP32 Connected:", data.id);
-
-        // 🔥 Send confirmation back
-        ws.send(JSON.stringify({
-          status: "connected",
-          message: "Welcome ESP32"
-        }));
-
-        return;
-      }
-
-      if (ws.deviceId) {
-        console.log("📩 Data from", ws.deviceId, ":", data);
-      }
-
-    } catch (err) {
-      console.log("Invalid JSON received");
-    }
-
-  });
-
-  ws.on("close", () => {
-    if (ws.deviceId) {
-      console.log("❌ ESP32 Disconnected:", ws.deviceId);
-    }
-  });
-
-});
 
 server.listen(process.env.PORT || 3000, () => {
   console.log("Server running 🚀");
